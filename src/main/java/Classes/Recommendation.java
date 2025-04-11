66c3e7f70b72eac630f4f4cb8ff93b42992c0fc4
aKha1ifa <131745823+aKha1ifa@users.noreply.github.com>
2025-04-11 22:30:53 +0200

    Update Recommendation.java

--- src/main/java/Classes/Recommendation.java
+++ src/main/java/Classes/Recommendation.java
@@ -2,6 +2,18 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
+
+
+
+
+
+
+//البلبل
+
+
+
+
+
 package Classes;
 
 import java.util.*;
